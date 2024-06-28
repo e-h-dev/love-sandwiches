3eb6c62019474f63ac6aed4340797f4ddb672976
Eli Hammond <ebaal8226@gmail.com>
2024-06-28 16:34:55 +0000

    add while loop to reoeat program with invalid input

--- run.py
+++ run.py
@@ -25,26 +25,33 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
 
-    data_str = input("Enter your data here: ")
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    sales_data = data_str.split(',')
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(',')
+        
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
 
 
 def validate_data(values):
     
     try:
-        print([int(value) for value in values])
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again. \n")
+        return False
 
+    return True
 
 get_sales_data()
